b1aab6b2871ac1b632c30768c1a932e691dbad31
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-08-04 07:22:09 +0800

    Add LogColorDefaults object to manage log color-coding
    Added a new object, LogColorDefaults, in the logstronaut package to handle the default color-coding for different logging levels. This provides a centralized place for managing log colors, improving the readability of logs and makes customization of log colors more straightforward.

---
+++ logstronaut/src/main/java/dev/jianastrero/logstronaut/default/LogColorDefaults.kt
@@ -0,0 +1,31 @@
+package dev.jianastrero.logstronaut.default
+
+import java.util.logging.Level
+
+object LogColorDefaults {
+
+    private const val SEVERE = "#b34045"
+    private const val WARNING = "#fecf6d"
+    private const val INFO = "#4091d7"
+    private const val CONFIG = "#2d884d"
+    private const val FINE = "#99e9eb"
+
+    fun colors(
+        finest: String = FINE,
+        finer: String = FINE,
+        fine: String = FINE,
+        config: String = CONFIG,
+        info: String = INFO,
+        warning: String = WARNING,
+        severe: String = SEVERE,
+    ) = mapOf(
+        Level.FINEST to finest,
+        Level.FINER to finer,
+        Level.FINE to fine,
+        Level.CONFIG to config,
+        Level.INFO to info,
+        Level.WARNING to warning,
+        Level.SEVERE to severe,
+    )
+
+}
