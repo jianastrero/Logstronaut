09e7304c84d5d266015cc4a737e6c3356d35b7cd
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-08-04 05:10:17 +0800

    Logstronaut Initial Commit

---
+++ example/src/main/java/dev/jianastrero/logstronaut/MainActivity.kt
@@ -0,0 +1,43 @@
+package dev.jianastrero.logstronaut
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.jianastrero.logstronaut.ui.theme.LogstronautTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LogstronautTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LogstronautTheme {
+        Greeting("Android")
+    }
+}
