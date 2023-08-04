047f7242ba6f6a73726a52b3c69ee017296cb1ad
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-08-04 13:15:31 +0800

    Remove 'depth' attribute from LogItem data class
    The 'depth' attribute was removed from LogItem.kt data class in the logstronaut module. This attribute was deemed unnecessary as it was not contributing to the main functionality of the logging information system, hence simplifying the LogItem class and improving the code's readability.

--- logstronaut/src/main/java/dev/jianastrero/logstronaut/model/LogItem.kt
+++ logstronaut/src/main/java/dev/jianastrero/logstronaut/model/LogItem.kt
@@ -3,7 +3,6 @@ package dev.jianastrero.logstronaut.model
 import dev.jianastrero.logstronaut.time.getCurrentUtcTimeAsString
 
 data class LogItem<T>(
-    val depth: Int,
     val message: T,
     val time: String = getCurrentUtcTimeAsString()
 )
