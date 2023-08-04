eea11e592c734a1cc4701b295a68f4553ef9b617
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-08-05 04:42:20 +0800

    Add maxMessageLength method to LogItem class
    A new function named 'maxMessageLength' was added to 'LogItem.kt'. This function calculates the maximum message length whether it's an array, collection or map. This change aims to provide more detailed log information to assist in troubleshooting by giving clearer insights into complex data types.

--- logstronaut/src/main/java/dev/jianastrero/logstronaut/model/LogItem.kt
+++ logstronaut/src/main/java/dev/jianastrero/logstronaut/model/LogItem.kt
@@ -7,3 +7,21 @@ data class LogItem<T>(
     val message: T,
     val time: String = getCurrentUtcTimeAsString()
 )
+
+fun LogItem<*>.maxMessageLength(): Int =
+    when (message) {
+        is Collection<*> -> message.maxOf { it.maxMessageLength() }
+        is Array<*> -> message.maxOf { it.maxMessageLength() }
+        is Map<*, *> -> message.maxOf { it.maxMessageLength() }
+        else -> message.toString().length
+    }
+
+private fun <T> T.maxMessageLength(): Int =
+    when(this) {
+        is Collection<*> -> maxOf { it.maxMessageLength() }
+        is Array<*> -> maxOf { it.maxMessageLength() }
+        is Map<*, *> -> {
+            maxOf(keys.maxOf { it.maxMessageLength() }, values.maxOf { it.maxMessageLength() })
+        }
+        else -> toString().length
+    }
