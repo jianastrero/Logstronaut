5bfd559cfd5f4417fb560d752b85ea9477baefc9
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-08-04 11:23:57 +0800

    Implement new configurations in Logstronaut
    Added new configuration options to Logstronaut, allowing developers to adjust paddingStart, paddingEnd, logging levels, and color values. This provides more flexibility and customization in how the logs are presented.

---
+++ logstronaut/src/main/java/dev/jianastrero/logstronaut/Logstronaut.kt
@@ -0,0 +1,42 @@
+package dev.jianastrero.logstronaut
+
+import dev.jianastrero.logstronaut.default.LogColorDefaults
+import java.util.logging.Level
+
+object Logstronaut {
+
+    internal var paddingStart = 1
+    internal var paddingEnd = 10
+    internal var level = Level.ALL
+    internal var colors: Map<Level, String> = LogColorDefaults.colors()
+
+    fun config(config: Config.() -> Unit) {
+        Config().apply(config)
+    }
+
+    class Config {
+        var paddingStart: Int
+            get() = Logstronaut.paddingStart
+            set(value) {
+                Logstronaut.paddingStart = value
+            }
+
+        var paddingEnd: Int
+            get() = Logstronaut.paddingEnd
+            set(value) {
+                Logstronaut.paddingEnd = value
+            }
+
+        var level: Level
+            get() = Logstronaut.level
+            set(value) {
+                Logstronaut.level = value
+            }
+
+        var colors: Map<Level, String>
+            get() = Logstronaut.colors
+            set(value) {
+                Logstronaut.colors = value
+            }
+    }
+}
