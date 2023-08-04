7992724a7494f3016376643217e9d8222fc970b5
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-08-04 13:35:48 +0800

    Add 'depth' attribute to LogItem data class
    'depth' attribute was added to LogItem.kt data class in the logstronaut module. This additional attribute is required in order to provide more granular information about the state of executed process, enhancing the information detail of the logging system.

--- logstronaut/src/main/java/dev/jianastrero/logstronaut/model/LogItem.kt
+++ logstronaut/src/main/java/dev/jianastrero/logstronaut/model/LogItem.kt
@@ -3,6 +3,7 @@ package dev.jianastrero.logstronaut.model
 import dev.jianastrero.logstronaut.time.getCurrentUtcTimeAsString
 
 data class LogItem<T>(
+    val depth: Int,
     val message: T,
     val time: String = getCurrentUtcTimeAsString()
 )
