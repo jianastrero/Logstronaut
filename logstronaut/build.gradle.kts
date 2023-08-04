6bb506b6e511f51624b02e3a2cf3419ef8a1b8fa
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-08-04 09:50:49 +0800

    Add Debug Implementation for kotlin-reflect in build.gradle.kts
    Added Debug Implementation of 'org.jetbrains.kotlin:kotlin-reflect:1.9.0' in build.gradle.kts for the logstronaut project. This additional library is required for Kotlin's built-in reflection features and will enhance debugging capability making bug discovery and fixes more efficient.

--- logstronaut/build.gradle.kts
+++ logstronaut/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
+    debugImplementation("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
 
     testImplementation("junit:junit:4.13.2")
 
