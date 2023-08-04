ff4ecacf9e21f4c0d229f6650ed4b4188251104d
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-08-04 14:07:16 +0800

    Add additional call to nestedFunc1 in MainActivity
    An extra call to 'nestedFunc1()' has been added in 'MainActivity.kt'. This is done to prevent this function from appearing in the stack trace reports generated during debugging. Therefore, this change simplifies the logs making the debugging process more manageable.

--- example/src/main/java/dev/jianastrero/example/MainActivity.kt
+++ example/src/main/java/dev/jianastrero/example/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : ComponentActivity() {
         CoroutineScope(Dispatchers.Default).launch {
             nestedFunc1()
         }
+        nestedFunc1()
 
         setContent {
             LogstronautTheme {
