0610c9234fc26b53b3da87723a102f9938fde265
Jian James P. Astrero <jianjamesastrero@gmail.com>
2023-08-05 13:25:29 +0800

    Refactor logging logic and improve output readability
    Significant refactoring was performed on LogstronautLogger.kt and LogItem.kt to improve the readability of the log output. Functions were added to handle depth logic spacing for different data types (arrays, collections and maps), and methods in the LogItem class were updated to provide their string representation and calculate their maximum message length. These changes aim to enhance the legibility of logs and simplify debugging.

--- logstronaut/src/main/java/dev/jianastrero/logstronaut/logger/LogstronautLogger.kt
+++ logstronaut/src/main/java/dev/jianastrero/logstronaut/logger/LogstronautLogger.kt
@@ -3,6 +3,8 @@ package dev.jianastrero.logstronaut.logger
 import android.util.Log
 import dev.jianastrero.logstronaut.Logstronaut
 import dev.jianastrero.logstronaut.model.LogItem
+import dev.jianastrero.logstronaut.model.maxMessageLength
+import dev.jianastrero.logstronaut.model.stringMessage
 
 fun <T> T.logD() {
     val log = generateLog()
@@ -13,44 +15,40 @@ private fun <T> T.generateLog(): String {
     val list = listOf(
         generateTitle(),
         generateStackTrace(),
-        generateVariable(0)
+//        generateVariable(0)
     )
-    val length = list.maxOf { it.maxOf { item -> item.message.toString().length + item.depth * 4 } } +
+    val length = list.maxOf { it.maxMessageLength(it.depth) } +
             Logstronaut.paddingStart +
             Logstronaut.paddingEnd
+    var log: String = length.startLog()
 
-    var log = "╭${"─" * length}╮\n"
-    list.forEachIndexed { index, logItems ->
-        logItems.forEach { logItem ->
-            when (logItem.message) {
-                is List<*> -> {
-
-                }
-                else -> {
-                    log += logItem.message.toString().createLine(logItem.depth, length)
-                }
-            }
+    list.forEachIndexed { index, logItem ->
+        logItem.stringMessage(logItem.depth).forEach {
+            log += it.createLine(logItem.depth, length)
         }
+
         if (index != list.lastIndex) {
             log += length.createSeparator()
         }
     }
-    log += "╰${"─" * length}╯"
+
+    log += length.endLog()
 
     return log
 }
 
-private fun generateTitle(): List<LogItem<*>> {
+private fun Int.startLog() = "╭${"─" * this}╮\n"
+private fun Int.endLog() = "╰${"─" * this}╯"
+
+private fun generateTitle(): LogItem<*> {
     val currentThread = Thread.currentThread()
     val threadName = currentThread.name
     val threadId = currentThread.id
     val title = "Thread: $threadName ($threadId)"
-    return listOf(
-        LogItem(0, title)
-    )
+    return LogItem(0, title)
 }
 
-private fun generateStackTrace(): List<LogItem<*>> {
+private fun generateStackTrace(): LogItem<*> {
     val stackTrace = Thread.currentThread().stackTrace
     val list = mutableListOf<LogItem<*>>()
     var index = 0
@@ -72,16 +70,11 @@ private fun generateStackTrace(): List<LogItem<*>> {
             it.className.matches("kotlin.*".toRegex())
         ) return@forEach
 
-        list.add(
-            LogItem(
-                index,
-                "${it.className}.${it.methodName}(${it.fileName}:${it.lineNumber})"
-            )
-        )
+        list.add(LogItem(index, "${it.className}.${it.methodName}(${it.fileName}:${it.lineNumber})"))
 
         index++
     }
-    return list
+    return LogItem(0, list)
 }
 
 private fun <T> T.generateVariable(depth: Int): List<LogItem<*>> {
@@ -104,20 +97,11 @@ private fun <T> T.generateVariable(depth: Int): List<LogItem<*>> {
 private fun Collection<*>.generateCollection(depth: Int): List<LogItem<*>> = listOf(LogItem(depth, this))
 private fun Array<*>.generateArray(depth: Int) = listOf(LogItem(depth, toList()))
 
-        variables.forEach { variable ->
-            list.add(LogItem(variable.depth, "${variable.message}$comma", variable.time))
-        }
-    }
-    list.add(LogItem(depth, "]"))
-    return list
-}
-
-private fun Array<*>.generateArray(depth: Int) = toList().generateCollection(depth)
-
 private operator fun String.times(times: Int): String = repeat(times)
-private fun Int.tabs(): String = " " * (4 * this)
-private fun String.createLine(indent: Int, characters: Int): String {
-    val remainingSpace = characters - Logstronaut.paddingStart - length - (indent * 4)
-    return "│${" " * Logstronaut.paddingStart}${indent.tabs()}$this${" " * remainingSpace}│\n"
-}
+internal fun Int.tabs(): String = " " * (4 * this)
 private fun Int.createSeparator() = "├${"─" * this}┤\n"
+
+private fun String.createLine(indent: Int, maxCharacters: Int): String {
+    val remainingSpace = maxCharacters - Logstronaut.paddingStart - length - (indent * 4)
+    return "│${" " * Logstronaut.paddingStart}$this${" " * remainingSpace}│\n"
+}
