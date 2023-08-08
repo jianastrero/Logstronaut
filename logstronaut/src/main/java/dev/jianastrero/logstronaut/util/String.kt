6b1d9e2691a1e41a7660565df7ec3808fd1b7615
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-08 11:19:53 +0800

    Add string repeat and tab functions in String.kt
    Two utility functions have been added to String.kt file. These functions are an operator override to repeat a string a specified number of times, and a function to convert an integer to an equivalent number of tabs spaces. The changes are likely to be useful in formatting output for logs and any context where we need to repeat a string or create a properly indented string.

--- logstronaut/src/main/java/dev/jianastrero/logstronaut/util/String.kt
+++ logstronaut/src/main/java/dev/jianastrero/logstronaut/util/String.kt
@@ -1,5 +1,17 @@
 package dev.jianastrero.logstronaut.util
 
+/**
+ * Repeats the given string a specified number of times.
+ *
+ * @param times The number of times to repeat the string.
+ * @return The resulting string after being repeated.
+ */
 internal operator fun String.times(times: Int): String = repeat(times)
 
+/**
+ * Returns a string consisting of a number of tabs based on the value of the receiver integer.
+ *
+ * @receiver The integer value representing the number of tabs.
+ * @return A string consisting of the specified number of tabs.
+ */
 internal fun Int.tabs(): String = " " * (4 * this)
