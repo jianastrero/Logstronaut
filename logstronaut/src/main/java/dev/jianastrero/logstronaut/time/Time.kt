750de6ebe9cc1d69fe92db97458ddf757a4400ee
Jian James Astrero <jianjamesastrero@gmail.com>
2023-08-08 11:18:44 +0800

    Add UTC time output in Time.kt
    A new function `getCurrentUtcTimeAsString` was added to Time.kt. This function provides the current UTC time as a string, which will aid in displaying the exact timestamp for log entries. This facilitates easier tracking and debugging of events logged by the application.

--- logstronaut/src/main/java/dev/jianastrero/logstronaut/time/Time.kt
+++ logstronaut/src/main/java/dev/jianastrero/logstronaut/time/Time.kt
@@ -4,5 +4,10 @@ import java.time.ZoneOffset
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
+/**
+ * Returns the current UTC time as a string representation.
+ *
+ * @return the current UTC time as a string.
+ */
 fun getCurrentUtcTimeAsString(): String =
     ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT)
